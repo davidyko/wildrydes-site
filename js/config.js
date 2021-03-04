43cb3005f54a4e20820ac6aafbf42cebfcf217d0
davidyko <ko.david.y@gmail.com>
2021-03-04 13:57:43 -0800

    Updated config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_bt7GyrXg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3es8nie8srf4km7rcsfalp737u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
